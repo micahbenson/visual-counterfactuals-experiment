aa76103183df728f1b65a4ecd479a99d4e086d53
Micah Benson <micahb@scc-505.scc.bu.edu>
2024-12-05 18:38:50 -0500

    saved new eval script

---
+++ counterfactuals_original/keypoints-eval.py
@@ -0,0 +1,118 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from collections import defaultdict
+
+import numpy as np
+import torch
+
+from utils.common_config import (
+    get_test_dataset,
+    get_test_transform,
+)
+
+
+def compute_eval_metrics(
+    counterfactuals,
+    dataset,
+):
+    #This need to be adapted so that I can also measure a same kp across experiments
+    """
+    Compute eval metrics from the paper `Counterfactual Visual Explanations`.
+
+    We compute the Near-KP and Same-KP metric using the keypoint annotations
+    from the dataset. In particular, for each cell replacement, we find what
+    keypoints are present in the cell from the query and distractor image(s).
+    If there are keypoints present, we increment the Near-KP score, which means
+    we are replacing regions near important object parts. If the cell in the
+    query image and the cell in the distractor image have at least one keypoint
+    in common we increase the Same-KP metric. This means we are replacing
+    semantically consistent parts. We track the metrics on a sample-wise level,
+    and average the results when reporting numbers in the paper.
+
+    Results are returned as a dictionary.
+    """
+    # initialize results dict
+    results = {
+        "Same-KP": defaultdict(list),
+    }
+
+    # load keypoint annotations into memory once
+    n_parts = len(dataset.parts_name_index)
+    keypoints = torch.stack(
+        [
+            torch.from_numpy(dataset.__getitem__(index)["parts"])
+            for index in range(len(dataset))
+        ],
+        dim=0,
+    )  # n x n_parts x n_pix x n_pix (n_pix = 7)
+    #Idk what that comment references but might be a shape thing I have to deal with...
+    #but could also just be implicit and change when I adjust everything else... 
+
+    # evaluate all counterfactuals
+    for counterfactual in counterfactuals.values():
+        # gather query and distractor indexes
+        query_index = counterfactual["query_index"]
+        distractor_index = counterfactual["distractor_index"]
+
+        # gather keypoint annotations for query and distractor images
+        # of shape (n x n_parts x n_pix x n_pix)
+        query_keypoints = keypoints[query_index]
+        distractor_keypoints = keypoints[distractor_index]
+        if len(query_keypoints.shape) == 3:
+            query_keypoints = query_keypoints.unsqueeze(0)
+        if len(distractor_keypoints.shape) == 3:
+            distractor_keypoints = distractor_keypoints.unsqueeze(0)
+
+        # flatten the keypoint maps
+        query_keypoints = (
+            torch.permute(query_keypoints, (1, 0, 2, 3)).reshape(n_parts, -1).float()
+        )
+        distractor_keypoints = (
+            torch.permute(distractor_keypoints, (1, 0, 2, 3))
+            .reshape(n_parts, -1)
+            .float()
+        )
+
+        # measure near-kp and same-kp for each edit
+        for edit in counterfactual["edits"]:
+            query_cell, distractor_cell = edit[0], edit[1]
+            same_kp = torch.any(
+                torch.logical_and(
+                    query_keypoints[:, query_cell],
+                    distractor_keypoints[:, distractor_cell],
+                )
+            ).float()
+            results["Same-KP"][query_index].append(same_kp.item())
+
+    # average the results
+    single_edit = {}
+    all_edit = {}
+
+    single_edit["Same-KP"] = np.nanmean([res[0] for res in results["Same-KP"].values()])
+
+    all_edit["Same-KP"] = np.nanmean(
+        np.concatenate(list(res for res in results["Same-KP"].values()))
+    )
+
+    # report results for single-edit and all edits
+    results["single_edit"] = single_edit
+    results["all_edit"] = all_edit
+
+    return results
+
+
+path  = "path"
+counterfactuals = np.load(path)
+dataset = get_test_dataset(transform=get_test_transform())
+
+result = compute_eval_metrics(
+    counterfactuals,
+    dataset=dataset,
+)
+
+print("Eval results single edit: {}".format(result["single_edit"]))
+print("Eval results all edits: {}".format(result["all_edit"]))
